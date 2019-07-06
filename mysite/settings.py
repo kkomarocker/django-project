a53d5e610d6163cd52c05a17882c6fd373ede910
Jay.Lee <kkomarocker@gmail.com>
2019-07-06 19:47:14 -0400

    init db connection

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'client_encoding': 'UTF-8',
+        }
     }
 }
 
